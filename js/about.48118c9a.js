87f666a4643a66487af232d30821a1161fd16682
user-yun <xuyunfei.mail@qq.com>
2020-05-17 13:22:50 +0800

    init gh-pages

---
+++ js/about.48118c9a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"5f83":function(t,n,a){},f26d:function(t,n,a){"use strict";var s=a("5f83"),e=a.n(s);e.a},f820:function(t,n,a){"use strict";a.r(n);var s=function(){var t=this,n=t.$createElement,a=t._self._c||n;return a("div",{staticClass:"about"},[a("h1",[t._v("This is an about page")]),a("el-button",[t._v(t._s(t.$t("navbar.test")))])],1)},e=[],u=(a("f26d"),a("2877")),i={},o=Object(u["a"])(i,s,e,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=about.48118c9a.js.map
